89537c67463a2c786af8c09a802df94ab9464fd8
Megan Leich <mleich@athenian.org>
2020-04-21 01:37:08 +0000

    reference code finished

--- my_code.py
+++ my_code.py
@@ -2,9 +2,58 @@ from py_extras import change_stmt
 
 
 def make_change(cost, amount_given):
-    # Enter you code here
-    # Consider using variables that match the variables used in the call to change_stmt() below
+        
+    twenties = 0
+    tens = 0
+    fives = 0
+    ones = 0
+    quarters = 0
+    dimes = 0
+    nickels = 0
+    pennies = 0
 
+    # Making change in pennies (to avoid float math)
+    change = round(((amount_given - cost) * 100), 2)
+    print ("Your change is: "+ str(change) + " cents")
+
+    #How many $20 bills?
+    while change >= 2000:
+        twenties += 1
+        change -= 2000
+
+    # How many $10 bills?
+    while change >= 1000:
+        tens += 1
+        change -= 1000
+
+    # fives
+    while change >= 500:
+        fives += 1
+        change -= 500
+
+    # ones
+    while change >= 100:
+        ones += 1
+        change -= 100
+
+    #Coins
+    while change >= 25:
+        quarters += 1
+        change -= 25
+
+    while change >= 10:
+        dimes += 1
+        change -= 10
+
+    while change >= 5:
+        nickels += 1
+        change -= 5
+
+    while change >= .5:
+        pennies += 1
+        change -= 1
+
+    print (change)
     # This will be the final statement of make_change() and will return the appropriate string
     return change_stmt(twenties, tens, fives, ones, quarters, dimes, nickels, pennies)
 
@@ -12,8 +61,10 @@ def make_change(cost, amount_given):
 if __name__ == '__main__':
     # Test your code with this first 
     # Change the cost and paid values to try different inputs
-    cost = 45.33
-    paid = 77.22
+    cost = 60
+    paid = 117.24
+    #cost = float(input("What is the total cost? "))
+    #paid = float(input("How much did you pay? "))
     s = make_change(cost, paid)
     print(s)
 
